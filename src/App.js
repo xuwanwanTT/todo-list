cb8add8308f7e8f59ff6d3cb24d45d4f6b626694
xuwanwan <1054749044@qq.com>
2017-09-16 22:08:47 +0800

    基本框架完成

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
+  constructor(props){                   //设置state的初始值
+    super(props)                       //super(),相当于引入this
+    this.state = {                    
+      newTodo: 'test',                //newTodo变量存储输入框中的内容
+      todoList: [                   
+        {id:1,title:'第一个待办'}     //todoList变量存储输入的所有todo
+      ]
+    }
+  } 
+  render() {                        //渲染
+
+    let todos = this.state.todoList.map((item,index)=>{
+      return <li>{item.title}</li>
+    })
+    return (                        //return一段XML，如果是多行需要用小括号括起来
       <div className="App">
-        <h1>hello, world</h1>
+        <h1>我的待办</h1>
+        <div className="inputWrapper">
+          <input type="text" value={this.state.newTodo} />
+        </div>
+        <ol>
+          {todos}
+        </ol>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App;                //模块化，绑定接口
