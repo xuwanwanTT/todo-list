679f42433b672afb6e5cfbce10758a29e58e7f6e
xuwanwan <1054749044@qq.com>
2017-09-17 00:07:04 +0800

    使用defaultValue修改input的value

--- src/App.js
+++ src/App.js
@@ -9,28 +9,28 @@ class App extends Component {
   constructor(props){                   //设置state的初始值
     super(props)                       //super(),相当于引入this
     this.state = {                    
-      newTodo: 'test',                //newTodo变量存储输入框中的内容
-      todoList: [                   
-        {id:1,title:'第一个待办'},     //todoList变量存储输入的所有todo
-        {id:2,title:'第二个待办'}
-      ]
+      newTodo: '',                //newTodo变量存储输入框中的内容
+      todoList: []               //todoList变量存储输入的所有todo
     }
   } 
   render() {                        //渲染
 
     let todos = this.state.todoList.map((item,index)=>{
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>
       )
     })
+
+    console.log(todos)
     
     return (                        //return一段XML，如果是多行需要用小括号括起来
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">  
-          <TodoInput content={this.state.newTodo} />
+          <TodoInput content={this.state.newTodo}
+            onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>
           {todos}
@@ -38,6 +38,25 @@ class App extends Component {
       </div>
     )
   }
+  addTodo(event){
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;                //模块化，绑定接口
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react'
 export default class TodoInput extends Component {
   render(){
     return <input type="text" defaultValue={this.props.content}
-      onKeyPress={this.submit} />
+      onKeyPress={this.submit.bind(this)} />
+      //这里在执行事件时，是按照submit.call()方法调用函数
+      //此时的submit函数的this不是指向"this.props"中的this
+      //所以需要bind(this)
   }
   submit(e){
     if(e.key === 'Enter'){
-      console.log('enter')
+      this.props.onSubmit(e)
     }
   }
 }
