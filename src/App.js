7e55a76dc7139ee2610e4b0c55f0c97ed1992b7a
xuwanwan <1054749044@qq.com>
2017-09-17 00:42:20 +0800

    删除todo

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
     let todos = this.state.todoList.map((item,index)=>{
       return (
         <li key={index}>
-          <TodoItem todo={item} onToggle={this.toggle.bind(this)} />
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+            onDelete={this.delete.bind(this)} />
         </li>
       )
     })
@@ -60,6 +61,10 @@ class App extends Component {
       newTodo: '',
       todoList: this.state.todoList
     })
+  }
+  delete(event,todo){
+    todo.deleted = true
+    this.setState(this.state)
   }
 }
 
