9e77b10424786bb8ae340a78a267be89a67d8a81
xuwanwan <1054749044@qq.com>
2017-09-17 12:14:48 +0800

    初步完善TodoItem的css

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
   render(){
-    return <input type="text" value={this.props.content}
+    return <input type="text" value={this.props.content} placeholder="按ENTER键添加todo"
       onChange={this.changeTitle.bind(this)}
       onKeyPress={this.submit.bind(this)} />
       //这里在执行事件时，是按照submit.call()方法调用函数
