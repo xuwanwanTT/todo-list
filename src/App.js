95d91dfbe86e9193102587422881d7d513bad66d
xuwanwan <1054749044@qq.com>
2017-09-18 11:52:13 +0800

    删除localStorage功能

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import TodoInput from './TodoInput'       //将输入框封装成TodoInput组件
 import TodoItem from './TodoItem'         //将每条待办封装成TodoItem组件
 import 'normalize.css'                    //CSS reset的替代方案
 import './reset.css'                        //手动reset
-import * as localStore from './localStore'    //封装localStorage
 
 class App extends Component {
   constructor(props){                   //设置state的初始值
     super(props)                       //super(),相当于引入this
     this.state = {                    
       newTodo: '',                //newTodo变量存储输入框中的内容
-      todoList: localStore.load('todoList') || []   //todoList变量存储输入的所有todo
+      todoList: []   //todoList变量存储输入的所有todo
     }
   } 
   render() {                        //渲染
@@ -44,7 +43,7 @@ class App extends Component {
   }
 
   componentDidUpdate(){
-    localStore.save('todoList',this.state.todoList)
+    
   }   //理解“组件更新 == 数据更新” componentDidUpdate会在组件更新后调用
 
   toggle(e,todo){
