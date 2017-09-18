435b18e3b8ad815362739b14005bfadef50d55fa
xuwanwan <1054749044@qq.com>
2017-09-18 22:33:38 +0800

    优化onSignIn,onSignUp

--- src/App.js
+++ src/App.js
@@ -46,8 +46,8 @@ class App extends Component {
         {this.state.user.id ?
           null :
           <UserDialog
-            onSignUp={this.onSignUp.bind(this)}
-            onSignIn={this.onSignIn.bind(this)} />}
+            onSignUp={this.onSignInOrSignUp.bind(this)}
+            onSignIn={this.onSignInOrSignUp.bind(this)} />}
       </div>
     )
   }
@@ -59,13 +59,7 @@ class App extends Component {
     this.setState(stateCopy)
   }
 
-  onSignUp(user){
-    let stateCopy = JSON.parse(JSON.stringify(this.state))
-    stateCopy.user = user
-    this.setState(stateCopy)
-  }
-
-  onSignIn(user){
+  onSignInOrSignUp(user){
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = user
     this.setState(stateCopy)
