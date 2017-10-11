068aebc6daa33705d57eb7f664aa092031bb5f86
xuwanwan <1054749044@qq.com>
2017-10-11 22:27:26 +0800

    加入：登陆后自动刷新页面

--- src/App.js
+++ src/App.js
@@ -72,6 +72,7 @@ class App extends Component {
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = user
     this.setState(stateCopy)
+    window.location.reload()
   }
 
   componentDidUpdate(){
